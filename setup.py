2eafe5eb2b69b3104a33b817e3199d42b17f062b
techyshiv <shivangsaxena177@gmail.com>
2022-08-21 18:18:35 +0530

    Python-Package Uploaded

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 from setuptools import setup
-setup(
-    name="techyshiv-evenodd",
-    version="1.0.0",
-    description="Check Even and Odd for any given numbers or list and return True , False & list based on data type provided",
-    author="Shivang Saxena",
-    packages=["techyshiv_evenodd"],
-    zip_safe=False
-)
-
-from setuptools import setup
-
 def readme():
     with open('Readme.Md') as f:
         README = f.read()
